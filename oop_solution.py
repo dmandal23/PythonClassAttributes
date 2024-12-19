7f1a5ddf39d1075293889b89c1dce952239f1fb9
dmandal23 <61653857+dmandal23@users.noreply.github.com>
2024-12-19 14:49:24 +0545

    Add files via upload
    This is simple python programming class/attributes

---
+++ oop_solution.py
@@ -0,0 +1,19 @@
+class Mobile:
+    def __init__(self, userbrand, usermodel):
+        self.brand = userbrand 
+        self.model = usermodel
+
+      
+
+
+my_mobile = Mobile("Vivo","Vivo T3")
+
+print(my_mobile.brand)
+print(my_mobile.model)
+
+
+add_mobile = Mobile("Samsung","Galaxy A16")
+
+print(add_mobile.brand)
+print(add_mobile.model)
+
